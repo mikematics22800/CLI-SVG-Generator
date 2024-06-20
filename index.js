42daa75d0601e4df3a7cb31b945a6d8552ff5dea
Michael Medina <michaeljmedina22800@gmail.com>
2024-06-19 23:38:08 -0400

    created function to generate SVG file

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ import * as fs from 'node:fs';
 
 // Import questions array
 import questions from './questions.js';
+
+// Callback function to write SVG file with user inputs
+const generateSVG = (answers) => {
+  const shape = answers.shape
+  const shapeColor = answers.shapeColor
+  const contents = (
+    `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+      ${
+        shape == 'square' ? `<rect width='100%' height='100%' fill='${shapeColor}'/>`:
+        shape == 'circle' ? `<circle cx="100" cy="100" r="100" fill='${shapeColor}'/>`:
+        shape == 'triangle' ? `<polygon points="100,0 200,200 0,200" fill='${shapeColor}'/>`:
+        ''
+      }
+      <text x="50%" y="50%" font-size="40" fill="${answers.textColor}" dy=".3em" text-anchor="middle">${answers.text}</text>
+    </svg>`
+  )
+  fs.writeFile('./logo.svg', contents, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('SVG file created successfully!');
+    }
+  })
+}
